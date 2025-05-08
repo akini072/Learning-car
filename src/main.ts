e359780eec770959536a06823501750af35fee30
akin.ipek0505@gmail.com <akin.ipek0505@gmail.com>
2025-05-09 00:29:13 +0300

    refactored the module

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taxi - Backend')
+    .setDescription('Uber clone API.')
+    .setVersion('0.000000001')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(4000);
+}
+bootstrap();
